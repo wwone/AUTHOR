f4129bd349a38845218460836196be76ed1b6aee
Bob Swanson <40324245+wwone@users.noreply.github.com>
2020-07-27 14:07:10 -0500

    Update AuxiliaryMetadata.java
    Attempt to issue better error messages when the String constructor content is bad. Solution is "better" but not "best".

--- source/AuxiliaryMetadata.java
+++ source/AuxiliaryMetadata.java
@@ -8,9 +8,15 @@ import java.util.ArrayList;
 import java.util.TreeMap;
 import java.util.Map;
 
-
 /*
- * last edited 12/1/2018
+ * last edited 
+ * Sun 26 Jul 2020 09:40:55 AM CDT
+ *
+ * Have the Constructor do difficult things like issue messages
+ * to stderr, when given a bad metadata string. Try to alert the 
+ * user (cannot throw an exception....)
+ *
+ * remove dependency on old XMLUtils
  *
  * object for auxiliary metadata, such as 
  * that specified in the AUTHOR file, and to be
@@ -36,13 +42,27 @@ public class AuxiliaryMetadata extends AuxiliaryInformation
 		description = xx[0]; // first is additional description verbiage
 		keywords = xx[1]; // second is additional keywords
 		title = null;
-	}
+	} // end constructor with a single string
 
+	/*
+	 * this constructor ought to throw an exception in case the metadata is incorrectly
+	 * formatted. For now, we will spit an error out to stderr and make the metadata junky
+	 */
 	public AuxiliaryMetadata(String in,String tit)
 	{
 		title = tit;
 		String xx[] = in.split(":"); // 1 (one) colon delimiter
-		description = xx[0]; // first is additional description verbiage
-		keywords = xx[1]; // second is additional keywords
-	}
+		if (xx.length < 2)
+		{
+			// formatting wrong
+			System.err.println("ERROR in Section metadata for : " + tit + ", --" + in + "--"); 
+			description = "ERRORERROR";
+			keywords = "ERROR,ERROR,ERROR";
+		} 
+		else
+		{
+			description = xx[0]; // first is additional description verbiage
+			keywords = xx[1]; // second is additional keywords
+		}
+	} // end constructor with strings
 }
